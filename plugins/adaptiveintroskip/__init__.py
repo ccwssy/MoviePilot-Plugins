f6bf14fff21ca406e93fd9a0dd6b9746bafacd34
ccwssy <93508941+ccwssy@users.noreply.github.com>
2026-01-07 15:57:03 +0800

    Update __init__.py

--- plugins/adaptiveintroskip/__init__.py
+++ plugins/adaptiveintroskip/__init__.py
@@ -27,7 +27,7 @@ class AdaptiveIntroSkip(_PluginBase):
     # 插件作者
     plugin_author = "honue"
     # 作者主页
-    author_url = "https://github.com/honue"
+    author_url = "https://github.com/ccwssy"
     # 插件配置项ID前缀
     plugin_config_prefix = "AdaptiveIntroSkip_"
     # 加载顺序
